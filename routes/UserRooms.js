d99ea2423ac62746e57dbe370553646ff00a24c3
Nguyễn Huỳnh Bảo Tín <baotin22071998@gmail.com>
2023-12-28 15:58:57 +0700

    add API check user is in the room

--- routes/UserRooms.js
+++ routes/UserRooms.js
@@ -2,6 +2,24 @@ const express = require("express");
 const router = express.Router();
 const { Userrooms } = require("../models");
 
+//Check if user leaved the room or not
+router.get("/", async (req, res) => {
+    const { userId, roomId } = req.body;
+
+    if (!userId || !roomId) {
+        return res.status(400).json("User ID or room ID is not given!")
+    }
+
+    const userroom = await Userrooms.findOne({ UserId: userId, RoomId: roomId })
+
+    if (!userroom) {
+        return res.status(400).json("User is not in the room!")
+    }
+    return res.status(200).json(`User ${userId} is in room ${roomId}`)
+})
+
+module.exports = router;
+
 // add user to the room
 
 router.post("/", async (req, res) => {
