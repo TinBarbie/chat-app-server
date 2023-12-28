d0bd2abe13153dd311820a3a76073db531bf052e
TinBarbie <65520145+TinBarbie@users.noreply.github.com>
2023-12-28 17:35:36 +0700

    fix userroom API

--- routes/UserRooms.js
+++ routes/UserRooms.js
@@ -11,15 +11,15 @@ router.post("/", async (req, res) => {
         return res.status(400).json("User ID or room ID or userSocketId is not given!")
     }
 
-    const userroom = await Userrooms.findOne({ where: { UserId: userId, RoomId: roomId, userSocketId: userSocketId} })
+    const userroom = await Userrooms.findOne({ where: { UserId: userId, RoomId: roomId } })
 
-    if (userroom) {
-        return res.status(200).json("User is already in the room!")
+    if (!userroom) {
+        await Userrooms.create({ UserId: userId, RoomId: roomId, userSocketId: userSocketId })
+
+        return res.status(200).json("Userroom created successfully!")
     }
-
-    await Userrooms.create({ UserId: userId, RoomId: roomId, userSocketId: userSocketId })
-
-    return res.status(200).json("Userroom created successfully!")
+    
+    return res.status(200).json("User is already in the room!")
 })
 
 module.exports = router;
