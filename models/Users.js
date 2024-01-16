b353836abc5eeebeac39ab4dfb666ad7a7436972
Nguyễn Huỳnh Bảo Tín <baotin22071998@gmail.com>
2024-01-16 15:30:37 +0700

    handle unique log-in session for an account

--- models/Users.js
+++ models/Users.js
@@ -7,6 +7,10 @@ module.exports = (sequelize, DataTypes) => {
         password: {
             type: DataTypes.STRING,
             allowNull: false,
+        },
+        loggedIn: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: 0,
         }
     });
     Users.associate = (models) => {
--- routes/Users.js
+++ routes/Users.js
@@ -14,7 +14,7 @@ router.get("/auth", async (req, res) => {
         const validToken = verify(accessToken, "importantsecret");
         return res.status(200).json(validToken)
     } catch (err) {
-        return res.status(403).json("Access token is not valid!");
+        return res.status(403).json({error: "Access token is not valid!"});
     }
 })
 
@@ -25,14 +25,14 @@ router.post("/", async (req, res) => {
     const userName = user.username;
     const password = user.password;
 
-    const existingUser = await Users.findOne({ where: { name: userName } })
+    const userInstance = await Users.findOne({ where: { name: userName } })
 
-    if (existingUser) {
+    if (userInstance) {
         return res.status(401).json({ error: "Username is already exist!" })
     }
 
     if (!userName || !password) {
-        return res.status(400).json("User name or password is not given!")
+        return res.status(400).json({ error: "User name or password is not given!" })
     }
     await bcrypt.hash(password, 10).then((hash) => {
         Users.create({ name: userName, password: hash });
@@ -58,6 +58,10 @@ router.post("/login", async (req, res) => {
         return res.status(404).json({ error: "User Doesn't Exist" });
     }
 
+    if (userInstance.loggedIn) {
+        return res.status(401).json({ error: "User is already signed in. Try logout from other devices and log in again." })
+    }
+
     bcrypt.compare(password, userInstance.password).then(async (match) => {
         if (!match) res.status(403).json({ error: "Wrong Username And Password Combination" });
 
@@ -65,8 +69,29 @@ router.post("/login", async (req, res) => {
             { username: userInstance.name, id: userInstance.id },
             "importantsecret"
         );
+        userInstance.loggedIn = 1;
+        await userInstance.save();
         return res.status(200).json({ token: accessToken, username: userName, id: userInstance.id });
     });
 });
+
+router.post("/logout", async (req, res) => {
+    const userName = req.body.username;
+
+    if (!userName) {
+        return res.status(400).json({ error: "User name or password is not given!" })
+    }
+
+    const userInstance = await Users.findOne({ where: { name: userName } });
+
+    if (!userInstance) {
+        return res.status(404).json({ error: "User Doesn't Exist" });
+    }
+
+    userInstance.loggedIn = 0;
+    await userInstance.save();
+    return res.status(200).json("Logout Successfully!");
+});
+
 
 module.exports = router;
